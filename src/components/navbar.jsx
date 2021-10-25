d80036c08f26e8888ad457d83ac26e63d576dd28
JacksonTBailey <bailey95jackson@gmail.com>
2021-10-25 17:00:09 -0400

    Converted CSS to SCSS and making journal mobile first optimized

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 export function Navbar() {
-    const navbarArray= ["home","about","posts", "health"]
-
-    const mappedNavbar = navbarArray.map((navItem) =>{return <li key={navItem} className={`navbar navbar-${navItem}`}>{navItem}</li>})
+    const navbarArray= ["Latest","About","Posts"]
+    const mappedNavbar = navbarArray.map((navItem) =>{return <a key={navItem} className={`navbar navbar-${navItem}`} href= {`/${navItem}`}>{navItem}</a>})
+    const openNav /*https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp */
 
     return(
-            <ul className="navbar-ul">
-                {mappedNavbar}
-            </ul>
+        <nav className="site-navbar">
+            <div className="overlay">
+                <a href="javascript:void(0)" className="closebtn" onClick="closeNav()">&times;</a>
+                <div className="navbar-parent">
+                    {mappedNavbar}
+                </div>
+            </div>
+            <span onClick="openNav()">open</span>
+        </nav>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import {Navbar} from './components/navbar.jsx';
 import reportWebVitals from './reportWebVitals';
 
