731a640e5caa56acb16085f61cf6e29640a74eb5
Jackson Bailey <Bailey95jackson@gmail.com>
2021-10-26 07:50:18 -0400

    Added logic for navbar open/close hamburger for mobile

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 export function Navbar() {
     const navbarArray= ["Latest","About","Posts"]
     const mappedNavbar = navbarArray.map((navItem) =>{return <a key={navItem} className={`navbar navbar-${navItem}`} href= {`/${navItem}`}>{navItem}</a>})
-    const openNav /*https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp */
-
+    const [nav, setNav] = useState()
+    const websiteNavbar= () =>{
+        let navHeight=document.getElementsByClassName("site-navbar")
+        if(navHeight.style.height == "0%"){
+            setNav(navHeight.style.height="100%")
+        };
+        if(navHeight.style.height == "100%"){
+            setNav(navHeight.style.height="0%")
+        };
+    }
+    
     return(
         <nav className="site-navbar">
             <div className="overlay">
-                <a href="javascript:void(0)" className="closebtn" onClick="closeNav()">&times;</a>
+                <a href="javascript:void(0)" className="closebtn" onClick={websiteNavbar}>&times;</a>
                 <div className="navbar-parent">
                     {mappedNavbar}
                 </div>
             </div>
-            <span onClick="openNav()">open</span>
+            <span className="nav-hamburger" onClick={websiteNavbar}>&#9776; open</span>
         </nav>
     );
 }
